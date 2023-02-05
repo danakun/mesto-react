91166a375b11382e88d17753670ad4f36713976d
Dana Kun <dana.liepinsh@gmail.com>
2023-02-06 00:28:51 +0100

    deploy v

---
+++ src/components/Main.js
@@ -0,0 +1,74 @@
+
+import { useEffect, useState } from 'react';
+import Card from './Card';
+import { useContext } from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+
+
+function Main({ onEditAvatar, onEditProfile, onAddPlace, onCardClick, onCardLike, onCardDelete }) {
+  const { currentUser, cards } = useContext(CurrentUserContext);
+  // Переменные состояния информации профиля
+  //const [userName, setUserName] = useState('');
+  //const [userDescription, setUserDescription] = useState('');
+ // const [userAvatar, setUserAvatar] = useState('#');
+
+  // Переменные состояния массива карточек
+  //const [cards, setCards] = useState([]);
+
+  // Функция эффекта для данных профиля и карточки
+  // useEffect(() => {
+  //   Promise.all([api.getUserProfile(), api.getInitialCards()])
+  //     .then(([userData, cards]) => {
+  //       setUserName(userData.name);
+  //       setUserDescription(userData.about);
+  //       setUserAvatar(userData.avatar);
+  //       setCards(cards);
+  //     })
+  //     .catch((error) => console.log(`Ошибка: ${error}`));
+  // }, []);
+
+  return (
+    <main className="main">
+      <section className="profile container">
+        <div
+          className="profile__change-avatar-button"
+          aria-label="Открыть попап редактирования аватара"
+          tabIndex="1"
+          onClick={onEditAvatar}
+          style={{ backgroundImage: `url(${currentUser.avatar})` }}
+        ></div>
+        <div className="profile__info">
+          <h1 className="profile__name">{currentUser.name}</h1>
+          <p className="profile__job">{currentUser.about}</p>
+          <button
+            className="profile__edit-button"
+            aria-label="Изменить профиль"
+            type="button"
+            onClick={onEditProfile}
+          ></button>
+        </div>
+        <button
+          className="profile__add-button"
+          aria-label="Добавить"
+          type="button"
+          onClick={onAddPlace}
+        ></button>
+      </section>
+      <section className="elements container">
+        <ul className="photo-grid">
+          {cards.map((card) => (
+            <Card
+              card={card}
+              onCardClick={onCardClick}
+              onCardLike={onCardLike}
+              onCardDelete={onCardDelete}
+              key={card._id}
+            />
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
